ce7637bbe14ed00161927f67944d4a61b34c54e9
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-06 13:00:11 -1000

    finished user route

--- routes/users.js
+++ routes/users.js
@@ -43,4 +43,48 @@ router.post("/users/login", (req, res) => {
     });
 });
 
+router.post("/users/register", (req, res) => {
+  db.raw("SELECT * FROM users WHERE email = ?", [req.body.email])
+    .then(results => {
+      if (results.rows.length !== 0) {
+        throw new Error();
+      }
+      db.raw("INSERT INTO users (email,password) VALUES (?,?) RETURNING *", [
+        req.body.email,
+        req.body.password
+      ]).then(results => {
+        res.json(results.rows);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ message: "User already exists" });
+    });
+});
+
+router.put("/users/:user_id/forgot-password", (req, res) => {
+  db.raw("UPDATE users SET password = ? WHERE id = ?", [
+    req.body.password,
+    req.params.user_id
+  ]).then(results => {
+    res.json({ message: "New password created!" });
+  });
+});
+
+router.delete("/users/:user_id", (req, res) => {
+  db.raw("DELETE FROM users WHERE id = ?", [req.params.user_id])
+    .then(results => {
+      console.log(results);
+      if (results.rowCount === 0) {
+        throw new Error();
+      }
+      res.json({
+        message: `User id: ${req.params.user_id} successfully deleted`
+      });
+    })
+    .catch(err => {
+      res.status(400).json({ message: "User ID not found" });
+    });
+});
+
 module.exports = router;
