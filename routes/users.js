502eabb20f435c97f8a030e81600149c21ddae8d
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-05 21:57:41 -1000

    finished POST /users/login

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,45 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-router.get("/users", (req, res) => {
-  res.send("USERS smoke test");
+router.get("/users/:user_id", (req, res) => {
+  db.raw("SELECT * FROM users WHERE id = ?", [req.params.user_id])
+    .then(results => {
+      if (results.rows.length === 0) {
+        throw new Error();
+      }
+      console.log(results.rows);
+      res.json(results.rows);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: "User not found" });
+    });
+});
+
+router.post("/users/login", (req, res) => {
+  console.log(req.body);
+
+  let errMessage;
+  db.raw("SELECT * FROM users WHERE email = ?", [req.body.email])
+    .then(results => {
+      console.log(results.rows);
+      if (results.rows.length === 0) {
+        errMessage = "User not found";
+        throw new Error();
+      }
+
+      if (results.rows[0].password !== req.body.password) {
+        errMessage = "Incorrect password";
+        throw new Error();
+      }
+
+      console.log(results.rows);
+      res.json(results.rows);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json({ message: errMessage });
+    });
 });
 
 module.exports = router;
